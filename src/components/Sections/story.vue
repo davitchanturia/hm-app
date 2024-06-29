566e709b91da438cd9574d01f0cd7a311acefb75
davit.chanturia <chanturiadato@gmail.com>
2024-06-29 13:26:15 +0400

    style: story page markup

--- src/components/Sections/story.vue
+++ src/components/Sections/story.vue
@@ -1,3 +1,55 @@
 <template>
-  <section id="4" class="w-full h-screen bg-red-500">story</section>
+  <section
+    id="4"
+    class="w-full bg-black bg-[url('/src/assets/images/story-bg.png')] bg-no-repeat bg-cover"
+    :style="{ backgroundImage: `url('/src/assets/images/11.png')` }"
+  >
+    <div class="w-full max-w-[1084px] mx-auto grid grid-cols-2 gap-10 pt-64">
+      <!-- left div -->
+      <div class="col-span-1">
+        <h1
+          class="uppercase font-MangoGrotesqueBold text-[230px] font-bold text-white -translate-y-[70px]"
+        >
+          Story
+        </h1>
+
+        <div
+          class="font-InterTightLight font-light text-[26px] text-white -translate-y-[110px]"
+        >
+          On the first day of April, 3 years ago holistic design agency Holy
+          Motors opened for business.
+        </div>
+
+        <div
+          class="mt-8 text-4xl font-InterTightSemiBold font-semibold text-white -translate-y-[110px] uppercase"
+        >
+          Holy Motors is a holistic design agency helping brands leave their
+          mark on culture.
+        </div>
+      </div>
+
+      <!-- right div -->
+      <div
+        class="col-span-1 font-InterTightLight text-[26px] font-light text-white"
+      >
+        With multidisciplinary teams of designers, developers, strategists,
+        writers, creators, and artists, we work closely with founders and brand
+        owners through all stages of bringing their vision to life.
+        <br />
+        <br />
+        As most glorious human endeavours often are, Holy Motors was created out
+        of nothing when Georgia’s renowned creative duo Giorgi Avaliani and Nick
+        Kumbari joined their forces to be reborn. Bringing together years of
+        creative and design excellence.
+      </div>
+    </div>
+
+    <div
+      class="mt-10 text-white w-full max-w-[1084px] mx-auto flex justify-between pb-32"
+    >
+      <img src="/src/assets/images/our-story/logo-01.svg" />
+      <img src="/src/assets/images/our-story/logo-02.svg" />
+      <img src="/src/assets/images/our-story/logo-03.svg" />
+    </div>
+  </section>
 </template>
